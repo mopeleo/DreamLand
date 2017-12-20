bd10dc45cddfc83fabb9eb4d2f857d6d6672b896
mopeleo <mopeleo@126.com>
2017-12-20 10:37:51 +0800

    'update'

--- assets/scripts/battle.js
+++ assets/scripts/battle.js
@@ -51,6 +51,8 @@ cc.Class({
     // onLoad () {},
 
     start () {
+        // cc.director.getCollisionManager().enabled = true;  // 开启碰撞检测系统
+
         this.floorLabel.string = "floor : " + CONSTANT.BATTLE_SCENE_PARAM.floor;
         this.nextButton.node.on(
             cc.Node.EventType.TOUCH_END,
@@ -68,23 +70,35 @@ cc.Class({
         this.actorList = new Array(cell_x);
         for(var i = 0; i < cell_x; i++){
             this.actorList[i] = new Array(cell_y);
+            for(var j = 0;j < cell_y; j++){
+                this.actorList[i][j]=null;
+            }
         }
 
-        // 初始化单元格
-        for(var i = 0; i < cell_x; i++){
-            for(var j = 0;j < cell_y; j++){
-                var actor = cc.instantiate(this.prefabActor);
-                this.battleSprite.node.addChild(actor);
-                var px = (j - cell_x/2)*CONSTANT.BATTLE_SCENE_PARAM.cellWidth + CONSTANT.BATTLE_SCENE_PARAM.cellWidth/2;
-                var py = (cell_y/2-i)*CONSTANT.BATTLE_SCENE_PARAM.cellHeight - CONSTANT.BATTLE_SCENE_PARAM.cellHeight/2;
-                actor.setPosition(cc.p(px, py));
-                var spriteFrame = new cc.SpriteFrame();
-                var texture = cc.textureCache.addImage(cc.url.raw(CONSTANT.INIT_ACTOR_SPRITEFRAME.rawurl));
-                spriteFrame.setTexture(texture);
-                actor.getComponent(cc.Sprite).spriteFrame = spriteFrame;
-                actor.getComponent(cc.Sprite).spriteFrame.frameid = CONSTANT.INIT_ACTOR_SPRITEFRAME.frameid;
+        //初始化敌方位置
+        var count = 0;
+        var enemyNum = CONSTANT.BATTLE_SCENE_PARAM.getEnemyNum();
+        while(count < enemyNum){
+            var randomRow = Math.floor((Math.random() * (cell_y - 2)));  //最下方两行为分隔行与玩家行
+            var randomCol = Math.floor((Math.random() * cell_x));
+            var cell = this.getCell(randomRow, randomCol);
+            if(!cell){
+                cell = cc.instantiate(this.prefabActor);
+                var px = this.getPositionX(randomCol);
+                var py = this.getPositionY(randomRow);
+                cell.setPosition(cc.p(px, py));
 
-                this.actorList[i][j]=actor;
+                var spriteFrame = new cc.SpriteFrame();
+                var urlPath = CONSTANT.PIC_URL.enemydir + "0001" + ".jpg";
+                var texture = cc.textureCache.addImage(cc.url.raw(urlPath));
+                spriteFrame.setTexture(texture);
+                cell.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+                cell.on(cc.Node.EventType.TOUCH_END, this.attack, this);
+
+                this.battleSprite.node.addChild(cell);          //添加到场景
+                this.enemyList.push(cell);
+                this.actorList[randomRow][randomCol] = cell;
+                count++;
             }
         }
 
@@ -92,7 +106,12 @@ cc.Class({
         for(var i = 0; i < CONSTANT.BATTLE_SCENE_PARAM.playerActors.length ; i++){
             var playerActorId = CONSTANT.BATTLE_SCENE_PARAM.playerActors[i];
             if(playerActorId && playerActorId != ''){
-                var actor = this.getCell(cell_y - 1, i);
+                // var actor = this.getCell(cell_y - 1, i);
+                var actor = cc.instantiate(this.prefabActor);
+                var px = this.getPositionX(i);
+                var py = this.getPositionY(cell_y - 1);
+                actor.setPosition(cc.p(px, py));
+
                 actor.id = playerActorId;
                 var spriteFrame = new cc.SpriteFrame();
                 var urlPath = CONSTANT.PIC_URL.playerdir + playerActorId + ".jpg";
@@ -105,29 +124,11 @@ cc.Class({
                 actor.getChildByName("hp").getComponent(cc.Label).string = actorData.hp;
                 actor.getChildByName("atk").getComponent(cc.Label).string = actorData.atk;
 
+                this.battleSprite.node.addChild(actor);         //添加到场景
                 this.playerList.push(actor);
+                this.actorList[cell_y - 1][i] = actor;
             }
         }
-
-        //初始化敌方位置
-        var count = 0;
-        var enemyNum = CONSTANT.BATTLE_SCENE_PARAM.getEnemyNum();
-        while(count < enemyNum){
-            var randomRow = Math.floor((Math.random() * (cell_y - 2)));  //最下方两行为分隔行与玩家行
-            var randomCol = Math.floor((Math.random() * cell_x));
-            var cell = this.getCell(randomRow, randomCol);
-            if(cell.getComponent(cc.Sprite).spriteFrame.frameid == CONSTANT.INIT_ACTOR_SPRITEFRAME.frameid){
-                var spriteFrame = new cc.SpriteFrame();
-                var urlPath = CONSTANT.PIC_URL.enemydir + "0001" + ".jpg";
-                var texture = cc.textureCache.addImage(cc.url.raw(urlPath));
-                spriteFrame.setTexture(texture);
-                cell.getComponent(cc.Sprite).spriteFrame = spriteFrame;
-                cell.on(cc.Node.EventType.TOUCH_END, this.attack, this);
-                this.enemyList.push(cell);
-                count++;
-            }
-        }
-
     },
 
     attack: function(event){
@@ -139,15 +140,39 @@ cc.Class({
         }
         var ex = event.target.x;
         var ey = event.target.y;
+        /* 动态新增LABEL */
+        var hpNode = new cc.Node("hpText");
+        hpNode.x = ex;
+        hpNode.y = ey + 30;
+        hpNode.color = new cc.Color(226, 2, 69);
+        var label = hpNode.addComponent(cc.Label);
+        label.fontSize = 20;
+        label.string="";
+        this.node.addChild(hpNode);
+        /* 动态新增LABEL END*/
+
         this.playerList.sort(this.speedCompare);
         for(var i=0; i < this.playerList.length; i++){
             this.attacking++;
             var player = this.playerList[i];
-            var ox = player.x;
-            var oy = player.y;
             var self = this;
-            var finished = cc.callFunc(function(){self.attacking--;}, self);
-            var seq = cc.sequence(cc.delayTime(0.3*i), cc.moveTo(0.3, ex, ey), cc.moveTo(0.3, ox, oy), finished);
+            var actorData = ACTORS[player.id];
+            var blood = actorData.atk;
+            var fight = cc.callFunc(function(obj, bld){
+                hpNode.getComponent(cc.Label).string = bld;
+            }, self, blood);
+            var finished = cc.callFunc(function(){
+                self.attacking--;
+                if(self.attacking == 0){
+                    hpNode.getComponent(cc.Label).string = "";
+                }
+            }, self);
+            var seq = cc.sequence(
+                cc.delayTime(0.3*i),
+                cc.moveTo(0.3, ex, ey),
+                fight,
+                cc.moveTo(0.3, player.x, player.y),
+                finished);
             player.runAction(seq);
         }
     },
@@ -182,6 +207,16 @@ cc.Class({
             col = 0;
         }
         return this.actorList[row][col];
+    },
+
+    getPositionX:function(colIndex){
+        var cell_x = CONSTANT.BATTLE_SCENE_PARAM.getBattleCols();
+        return (colIndex - cell_x/2)*CONSTANT.BATTLE_SCENE_PARAM.cellWidth + CONSTANT.BATTLE_SCENE_PARAM.cellWidth/2;
+    },
+
+    getPositionY:function(rowIndex){
+        var cell_y = CONSTANT.BATTLE_SCENE_PARAM.getBattleRows();
+        return (cell_y/2 - rowIndex)*CONSTANT.BATTLE_SCENE_PARAM.cellHeight - CONSTANT.BATTLE_SCENE_PARAM.cellHeight/2;
     }
 
 });
